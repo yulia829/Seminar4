ffd30ccb21b5df8f6073515d11d7d9db8dd92293
Yulia <meshokyulia0002@gmail.com>
2023-08-30 18:31:57 +0300

    hw14

---
+++ hw4_1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("введите число A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число B");
+int b = Convert.ToInt32(Console.ReadLine());
+int n = a;
+
+for (int i = 1; i < b; i++)
+{
+n = n * a;
+}
+Console.WriteLine("Число в степени равно: " + n);
