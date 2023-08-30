fb3150a4e9bcd2cbfe851b3e5736486c1c66bfb5
Yulia <meshokyulia0002@gmail.com>
2023-08-30 19:01:38 +0300

    hw34

---
+++ hw4_3/Program.cs
@@ -0,0 +1,14 @@
+﻿int [] numbers = new int[8];
+Console.Write("[");
+
+for (int i = 0; i < numbers.Length; i++)
+ {
+    numbers [i] = new Random().Next(0, 99);
+    Console.Write(" " + Method (i) + " ");
+ }
+Console.Write("]");
+
+int Method (int a)
+{
+    return numbers[a];
+}
