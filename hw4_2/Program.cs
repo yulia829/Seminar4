8eff09de9f65659e8afc0c7262ec042db7440a9c
Yulia <meshokyulia0002@gmail.com>
2023-08-30 18:35:56 +0300

    hw24

---
+++ hw4_2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int i = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+while (i > 0)
+{
+int num = i % 10;
+i = i / 10;
+sum = sum + num;
+}
+Console.WriteLine("Сумма цифр : " + sum);
